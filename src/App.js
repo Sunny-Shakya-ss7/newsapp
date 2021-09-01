bbf969c52ca33281b3879bfddc8b5cf4b98b57ab
Sunny Shakya <51647092+Sunny-Shakya-ss7@users.noreply.github.com>
2021-09-01 21:57:41 +0545

    Refactoring updateNews()

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import News from "./Components/News";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 export default class App extends Component {
+  pageSize = 15;
+
   render() {
     return (
       <div>
@@ -14,7 +16,7 @@ export default class App extends Component {
             <Route exact path="/General">
               <News
                 key="General"
-                pageSize={6}
+                pageSize={this.pageSize}
                 country="in"
                 category="General"
               />
@@ -22,7 +24,7 @@ export default class App extends Component {
             <Route exact path="/Business">
               <News
                 key="Business"
-                pageSize={6}
+                pageSize={this.pageSize}
                 country="in"
                 category="Business"
               />
@@ -30,29 +32,39 @@ export default class App extends Component {
             <Route exact path="/Entertainment">
               <News
                 key="Entertainment"
-                pageSize={6}
+                pageSize={this.pageSize}
                 country="in"
                 category="Entertainment"
               />
             </Route>
             <Route exact path="/Health">
-              <News key="Health" pageSize={6} country="in" category="Health" />
+              <News
+                key="Health"
+                pageSize={this.pageSize}
+                country="in"
+                category="Health"
+              />
             </Route>
             <Route exact path="/Science">
               <News
                 key="Science"
-                pageSize={6}
+                pageSize={this.pageSize}
                 country="in"
                 category="Science"
               />
             </Route>
             <Route exact path="/Sports">
-              <News key="Sports" pageSize={6} country="in" category="Sports" />
+              <News
+                key="Sports"
+                pageSize={this.pageSize}
+                country="in"
+                category="Sports"
+              />
             </Route>
             <Route exact path="/Technology">
               <News
                 key="Technology"
-                pageSize={6}
+                pageSize={this.pageSize}
                 country="in"
                 category="Technology"
               />
