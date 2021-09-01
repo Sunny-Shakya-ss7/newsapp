8c320b78114354c5c1d72fc45cf0fd3df38a1d2f
Sunny Shakya <51647092+Sunny-Shakya-ss7@users.noreply.github.com>
2021-09-01 20:53:30 +0545

    Adding Categories & propTypes

--- src/App.js
+++ src/App.js
@@ -3,12 +3,11 @@ import NavBar from "./Components/NavBar";
 import News from "./Components/News";
 
 export default class App extends Component {
-  name = "Sunny";
   render() {
     return (
       <div>
         <NavBar />
-        <News pageSize={6} />
+        <News pageSize={6} country="in" category="Sports" />
       </div>
     );
   }
--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -38,6 +38,41 @@ export class NavBar extends Component {
                     About
                   </a>
                 </li>
+                <li className="nav-item">
+                  <a className="nav-link" aria-current="page" href="/">
+                    Business
+                  </a>
+                </li>
+                <li className="nav-item">
+                  <a className="nav-link" aria-current="page" href="/">
+                    Entertainment
+                  </a>
+                </li>
+                <li className="nav-item">
+                  <a className="nav-link" aria-current="page" href="/">
+                    General
+                  </a>
+                </li>
+                <li className="nav-item">
+                  <a className="nav-link" aria-current="page" href="/">
+                    Health
+                  </a>
+                </li>
+                <li className="nav-item">
+                  <a className="nav-link" aria-current="page" href="/">
+                    Science
+                  </a>
+                </li>
+                <li className="nav-item">
+                  <a className="nav-link" aria-current="page" href="/">
+                    Sports
+                  </a>
+                </li>
+                <li className="nav-item">
+                  <a className="nav-link" aria-current="page" href="/">
+                    Technology
+                  </a>
+                </li>
               </ul>
             </div>
           </div>
