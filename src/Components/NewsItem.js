1a46ea9c9eb017e3611a4563e8e624d08b9b7e6e
Sunny Shakya <51647092+Sunny-Shakya-ss7@users.noreply.github.com>
2021-09-01 21:34:15 +0545

    Adding time, author and news source

--- src/Components/NewsItem.js
+++ src/Components/NewsItem.js
@@ -2,10 +2,17 @@ import React, { Component } from "react";
 
 export class NewsItem extends Component {
   render() {
-    let { title, description, imageUrl, newsUrl } = this.props;
+    let { title, description, imageUrl, newsUrl, author, date, source } =
+      this.props;
     return (
       <div className="my-3">
         <div className="card">
+          <span
+            class="position-absolute top-0 translate-middle badge rounded-pill bg-danger"
+            style={{ zIndex: 1, left: "90%" }}
+          >
+            {source}
+          </span>
           <img
             src={
               imageUrl
@@ -17,7 +24,14 @@ export class NewsItem extends Component {
           />
           <div className="card-body">
             <h5 className="card-title">{title}...</h5>
+
             <p className="card-text">{description}...</p>
+            <p className="card-text">
+              <small className="text-muted">
+                By {author ? author : "unknown"} on{" "}
+                {new Date(date).toUTCString()}
+              </small>
+            </p>
             <a
               href={newsUrl}
               target="_blank"
